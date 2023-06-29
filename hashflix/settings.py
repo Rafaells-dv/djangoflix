677934092b27d49036cda045da92df984251e2d5
Rafael <rafaelleonardodasilva@gmail.com>
2023-06-29 15:52:20 -0300

    test

--- hashflix/settings.py
+++ hashflix/settings.py
@@ -86,13 +86,14 @@ DATABASES = {
     }
 }
 
+
 import dj_database_url
 import os
 
-DATABASE_URL = os.get_env('DATABASE_URL')
+DATABASE_URL = os.getenv('DATABASE_URL')
 if DATABASE_URL:
     DATABASES = {
-        'default': dj_database_url.config(default=DATABASE_URL, conn_max_age=1800)
+        'default': dj_database_url.parse(os.environ.get("DATABASE_URL"))
     }
 
 # Password validation
